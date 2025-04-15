22d7206d5b2fba31cffda7fc78fe3fd11b15af14
JAS0N-SMITH <huchknows@gmail.com>
2025-04-15 19:30:32 -0400

    feat: added new split_nodes functions for images and links

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -21,6 +21,7 @@ Functions:
 """
 
 from textnode import TextType, TextNode
+from extractor import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     """
@@ -52,3 +53,96 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)  # Append non-TextNode objects as-is
 
     return new_nodes
+
+# Now that we have the extraction functions, we will need to be able to split raw markdown text into TextNodes based on images and links.
+# '''
+
+
+def split_nodes_image(old_nodes):
+    """
+    Splits a list of text nodes into smaller nodes based on markdown images.
+
+    Args:
+        old_nodes (list): A list of TextNode objects to process.
+
+    Returns:
+        list: A new list of TextNode objects with the images extracted and types assigned.
+    """
+    new_nodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            if not node.text:  # Preserve empty TextNode
+                new_nodes.append(node)
+                continue
+
+            matches = extract_markdown_images(node.text)
+            if matches:
+                remaining_text = node.text
+                for alt_text, url in matches:
+                    pre_text, remaining_text = remaining_text.split(
+                        f"![{alt_text}]({url})", 1)
+                    if pre_text:
+                        new_nodes.append(TextNode(pre_text, TextType.TEXT))
+                    new_nodes.append(TextNode(alt_text, TextType.IMAGE))
+                    new_nodes.append(TextNode(url, TextType.TEXT))
+                if remaining_text:
+                    new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)  # Append non-TextNode objects as-is
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    """
+    Splits a list of text nodes into smaller nodes based on markdown links.
+
+    Args:
+        old_nodes (list): A list of TextNode objects to process.
+
+    Returns:
+        list: A new list of TextNode objects with the links extracted and types assigned.
+    """
+    new_nodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            if not node.text:  # Preserve empty TextNode
+                new_nodes.append(node)
+                continue
+
+            matches = extract_markdown_links(node.text)
+            if matches:
+                remaining_text = node.text
+                for anchor_text, url in matches:
+                    pre_text, remaining_text = remaining_text.split(
+                        f"[{anchor_text}]({url})", 1)
+                    if pre_text:
+                        new_nodes.append(TextNode(pre_text, TextType.TEXT))
+                    new_nodes.append(TextNode(anchor_text, TextType.LINK))
+                    new_nodes.append(TextNode(url, TextType.TEXT))
+                if remaining_text:
+                    new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)  # Append non-TextNode objects as-is
+
+    return new_nodes
+
+
+# Example usage:
+# node = TextNode(
+#     "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+#     TextType.TEXT,
+# )
+# new_nodes = split_nodes_link([node])
+# [
+#     TextNode("This is text with a link ", TextType.TEXT),
+#     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+#     TextNode(" and ", TextType.TEXT),
+#     TextNode(
+#         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+#     ),
+# ]
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -3,7 +3,7 @@ Unit tests for the splitnodes module, specifically testing the split_nodes_delim
 """
 
 import unittest
-from splitnodes import split_nodes_delimiter
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestSplitNodes(unittest.TestCase):
@@ -63,6 +63,106 @@ class TestSplitNodes(unittest.TestCase):
 
         expected_nodes = [
             TextNode("", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_image(self):
+        """
+        Test splitting text with markdown image syntax.
+        """
+        node = TextNode(
+            "This is an image ![alt text](https://example.com/image.png) in text", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+
+        expected_nodes = [
+            TextNode("This is an image ", TextType.TEXT),
+            TextNode("alt text", TextType.IMAGE),
+            TextNode("https://example.com/image.png", TextType.TEXT),
+            TextNode(" in text", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_image_multiple(self):
+        """
+        Test splitting text with multiple markdown image syntaxes.
+        """
+        node = TextNode(
+            "Image1 ![alt1](url1) and Image2 ![alt2](url2)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+
+        expected_nodes = [
+            TextNode("Image1 ", TextType.TEXT),
+            TextNode("alt1", TextType.IMAGE),
+            TextNode("url1", TextType.TEXT),
+            TextNode(" and Image2 ", TextType.TEXT),
+            TextNode("alt2", TextType.IMAGE),
+            TextNode("url2", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_image_malformed(self):
+        """
+        Test splitting text with malformed markdown image syntax.
+        """
+        node = TextNode(
+            "This is a malformed image ![alt text](url", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+
+        expected_nodes = [
+            TextNode(
+                "This is a malformed image ![alt text](url", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_link(self):
+        """
+        Test splitting text with markdown link syntax.
+        """
+        node = TextNode(
+            "This is a link [example](https://example.com) in text", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+
+        expected_nodes = [
+            TextNode("This is a link ", TextType.TEXT),
+            TextNode("example", TextType.LINK),
+            TextNode("https://example.com", TextType.TEXT),
+            TextNode(" in text", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_link_multiple(self):
+        """
+        Test splitting text with multiple markdown link syntaxes.
+        """
+        node = TextNode(
+            "Link1 [text1](url1) and Link2 [text2](url2)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+
+        expected_nodes = [
+            TextNode("Link1 ", TextType.TEXT),
+            TextNode("text1", TextType.LINK),
+            TextNode("url1", TextType.TEXT),
+            TextNode(" and Link2 ", TextType.TEXT),
+            TextNode("text2", TextType.LINK),
+            TextNode("url2", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_link_malformed(self):
+        """
+        Test splitting text with malformed markdown link syntax.
+        """
+        node = TextNode("This is a malformed link [text](url", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+
+        expected_nodes = [
+            TextNode("This is a malformed link [text](url", TextType.TEXT),
         ]
 
         self.assertEqual(new_nodes, expected_nodes)
